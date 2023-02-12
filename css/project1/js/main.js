13f5e8b5817a70aacaea6364e13e989fdde3dfcd
alakhpandya <alakhpandya@gmail.com>
2023-02-12 15:08:56 +0530

    feb10 commit

---
+++ css/project1/js/main.js
@@ -0,0 +1,9 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    items:1,
+    // autoplay: true,
+    // autoplayTimeout:1000,
+    // autoplayHoverPause: true
+})
